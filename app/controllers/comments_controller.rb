8d9c30d2e53b002a8307cf7177d993ceea933260
Rhiana Heath <rhiana@heath.cc>
2013-11-07 09:22:46 +1100

    comments can be submitted via ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create!(params[:comment])
-		redirect_to @post
+		redirect_to do |format|
+			format.html { redirect_to @post }
+			format.js
 	end
 end
