ba2129e4727a2c5140449fe61de8c7130efb1151
Rhiana Heath <rhiana.heath@reinteractive.net>
2018-11-30 11:08:02 +1100

    updating config and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,24 +8,23 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131015085954) do
+ActiveRecord::Schema.define(version: 20131015085954) do
 
-  create_table "comments", :force => true do |t|
-    t.integer  "post_id"
-    t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
-
-  create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "posts", force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
